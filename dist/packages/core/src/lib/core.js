7294c68afcd0ab457afb962fb464864fbe7551a6
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2024-10-27 18:00:37 +0000

    feat: testing ci release

---
+++ dist/packages/core/src/lib/core.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "core", {
+    enumerable: true,
+    get: function() {
+        return core;
+    }
+});
+function core() {
+    return 'core';
+}
+
+//# sourceMappingURL=core.js.map
