981afcb0518e25e0d85388ea57f87ee0936fd1dc
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2024-10-27 21:40:47 +0000

    feat: fixing changes

---
+++ packages/docs-site/next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from 'next'
+import nextra from 'nextra'
+ 
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+
+const nextConfig:NextConfig = {
+
+}
+ 
+export default withNextra(nextConfig)
+ 
+// If you have other Next.js configurations, you can pass them as the parameter:
+// export default withNextra({ /* other next.js config */ })
