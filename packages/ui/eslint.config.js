6a357617b18c0a70c022a2b27cb6d8d3bc57e313
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2024-10-30 23:29:47 +0000

    feat: trying to add shadcn

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,22 +1,12 @@
+const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
 
 module.exports = [
   ...baseConfig,
+  ...nx.configs['flat/react'],
   {
-    files: ['**/*.json'],
-    rules: {
-      '@nx/dependency-checks': [
-        'error',
-        {
-          ignoredFiles: [
-            '{projectRoot}/eslint.config.{js,cjs,mjs}',
-            '{projectRoot}/vite.config.{js,ts,mjs,mts}',
-          ],
-        },
-      ],
-    },
-    languageOptions: {
-      parser: require('jsonc-eslint-parser'),
-    },
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
   },
 ];
