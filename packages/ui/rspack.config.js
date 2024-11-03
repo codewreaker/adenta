2b04b0d7ab5a8001042e78e12180f1cd1dddb47d
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2024-11-03 19:07:48 +0000

    progress: setup shadcn with rspack

--- packages/ui/rspack.config.js
+++ packages/ui/rspack.config.js
@@ -4,15 +4,16 @@ const {globSync} = require('glob')
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
 const path = require('node:path');
 
+
 const globFiles = globSync([
   "**/*.{js,ts,tsx}"
 ],{
   cwd: path.resolve(__dirname, './'),
 });
 
-const entries = globFiles.reduce((acc, filePath) => {
-  const entryName = path.relative("packages/ui/", filePath).replace(/\.[jt]sx?$/, "");
-  acc[entryName] = `./${filePath}`;
+const entries = globFiles.reduce((acc, filePath, idx) => {
+  const entryName = path.resolve(__dirname, filePath).replace(/\.[jt]sx?$/, "");
+  acc[entryName] = path.resolve(__dirname, filePath);
   return acc;
 }, {});
 
@@ -24,16 +25,33 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
       ...entries
     },
     output: {
-      ...config.output
+      ...config.output,
+      filename: "[name].js",
+      libraryTarget: "module",
+      chunkFormat: "module",
     },
     experiments: {
+      ...config.experiments,
       outputModule: true,
+    },
+    module:{
+      ...config.module,
+      rules:[
+        ...config.module.rules,
+        {
+          test: /\.node$/, // Exclude .node files
+          use: "ignore-loader",
+        }
+      ]
     },
     resolve: {
       ...config.resolve,
       alias: {
         "@": path.resolve(__dirname, "./src")
       }
-    }
+    },
+    optimization: {
+      minimize: false, // Keep individual files readable
+    },
   };
 });
