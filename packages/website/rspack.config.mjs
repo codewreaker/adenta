88a02faefa3de71a98469759ae7d21328db01305
Israel Agyeman-Prempeh <israel.agyeman.prempeh@gmail.com>
2025-06-18 23:07:55 +0100

    chore: adding supabase

--- packages/website/rspack.config.mjs
+++ packages/website/rspack.config.mjs
@@ -1,15 +1,15 @@
 import { composePlugins, withNx, withReact } from '@nx/rspack';
-import { tanstackRouter } from '@tanstack/router-plugin/rspack'
+//import { tanstackRouter } from '@tanstack/router-plugin/rspack'
 
 export default composePlugins(withNx(), withReact(), (config) => {
   return {
     ...config,
     plugins: [
       ...config.plugins,
-      tanstackRouter({
-        target: 'react',
-        autoCodeSplitting: true,
-      })
+      // tanstackRouter({
+      //   target: 'react',
+      //   autoCodeSplitting: true,
+      // })
     ],
     output: {
       ...config.output,
